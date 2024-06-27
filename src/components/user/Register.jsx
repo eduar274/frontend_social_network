683adc9e6448fc7292a4b540e3baac00317fa1a9
inesmariao <inesmao.misena@gmail.com>
2024-06-26 19:24:13 -0500

    required in inputs form

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -12,23 +12,23 @@ export const Register = () => {
         <form className="register-form">
           <div className="form-group">
             <label htmlFor="name">Nombres</label>
-            <input type="text" name="name"/>
+            <input type="text" name="name" required />
           </div>
           <div className="form-group">
             <label htmlFor="last_name">Apellidos</label>
-            <input type="text" name="last_name"/>
+            <input type="text" name="last_name" required/>
           </div>
           <div className="form-group">
             <label htmlFor="nick">Nick</label>
-            <input type="text" name="nick"/>
+            <input type="text" name="nick" required/>
           </div>
           <div className="form-group">
             <label htmlFor="email">Correo Electrónico</label>
-            <input type="email" name="email"/>
+            <input type="email" name="email" required/>
           </div>
           <div className="form-group">
             <label htmlFor="password">Contraseña</label>
-            <input type="password" name="password"/>
+            <input type="password" name="password" required/>
           </div>
 
           <input type="submit" value="Regístrate" className="btn btn-success" />
