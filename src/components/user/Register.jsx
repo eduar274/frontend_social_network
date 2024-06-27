69c3a099bee2ca337f345f19619fcbc5f900bbb6
inesmariao <inesmao.misena@gmail.com>
2024-06-26 19:21:22 -0500

    Register.jsx component with form OK

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -2,6 +2,38 @@
 
 export const Register = () => {
   return (
-    <div>Register</div>
+    <>
+      <header className="content__header content__header--public">
+          <h1 className="content__title">Registro</h1>
+      </header>
+
+      {/* Formulario de Registro*/}
+      <div className="content__posts">
+        <form className="register-form">
+          <div className="form-group">
+            <label htmlFor="name">Nombres</label>
+            <input type="text" name="name"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="last_name">Apellidos</label>
+            <input type="text" name="last_name"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="nick">Nick</label>
+            <input type="text" name="nick"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Correo Electrónico</label>
+            <input type="email" name="email"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Contraseña</label>
+            <input type="password" name="password"/>
+          </div>
+
+          <input type="submit" value="Regístrate" className="btn btn-success" />
+        </form>
+      </div>
+    </>
   )
 }
